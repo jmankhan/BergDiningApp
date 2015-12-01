afa88007e6043dafe527f1e6d292bf8f6dba913e
paihuolong <paihuolong@gmail.com>
2015-12-01 15:41:09 -0500

    Added different color icons for different stations and code for a custom AlertDialog for item details.

--- app/src/main/java/muhlenberg/edu/bergdining/MenuListAdapter.java
+++ app/src/main/java/muhlenberg/edu/bergdining/MenuListAdapter.java
@@ -9,6 +9,7 @@ import android.graphics.PorterDuff;
 import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -56,9 +57,12 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.Custom
             String message = "Calories:\t" + item.facts.calories + "\n"
                     + "Carbs:\t" + item.facts.carbo + "\n"
                     + "Fat:\t" + item.facts.fat;
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             AlertDialog dialog = new AlertDialog.Builder(context)
                     .setTitle("Details - " + item.name)
-                    .setMessage(message)
+               //     .setMessage(message)
+                    .setView(inflater.inflate(R.layout.item_details, null, false))
                     .create();
 
             dialog.show();
@@ -88,6 +92,20 @@ public class MenuListAdapter extends RecyclerView.Adapter<MenuListAdapter.Custom
         if(menu.items.get(position).facts.station.contains("Wildfire"))
             holder.imageView.getDrawable().setColorFilter(Color.rgb(237, 231, 159), PorterDuff.Mode.MULTIPLY);
 
+        else if(menu.items.get(position).facts.station.contains("Crouton"))
+            holder.imageView.getDrawable().setColorFilter(Color.rgb(0, 231, 0), PorterDuff.Mode.MULTIPLY);
+
+        else if(menu.items.get(position).facts.station.contains("Mangia"))
+            holder.imageView.getDrawable().setColorFilter(Color.rgb(237, 231, 231), PorterDuff.Mode.MULTIPLY);
+
+        else if(menu.items.get(position).facts.station.contains("Magellan"))
+            holder.imageView.getDrawable().setColorFilter(Color.rgb(237, 0, 237), PorterDuff.Mode.MULTIPLY);
+
+        else if(menu.items.get(position).facts.station.contains("Chef"))
+            holder.imageView.getDrawable().setColorFilter(Color.rgb(237, 237, 237), PorterDuff.Mode.MULTIPLY);
+
+        else
+            holder.imageView.getDrawable().setColorFilter(Color.rgb(0, 0, 0), PorterDuff.Mode.MULTIPLY);
     }
 
     @Override
